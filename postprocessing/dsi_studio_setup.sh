e82d2ed712021f3da8c1b3c2518d113f4f489d49
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2018-05-10 10:43:05 -0500

    More streamlining, add FSL, add AC

--- postprocessing/dsi_studio_setup.sh
+++ postprocessing/dsi_studio_setup.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+
+# DSI Studio command line binary
 export DSI_STUDIO=/Applications/dsi_studio.app/Contents/MacOS/dsi_studio
+
+# Options for tractography
 export DSI_OPTION_STRING="--method=0 --fiber_count=100000 --threshold_index=nqa --fa_threshold=0.1 --initial_dir=0 --step_size=0 --smoothing=1 --min_length=30 --max_length=300 --random_seed=0 --seed_plan=0 --interpolation=0"
+
+# FSL configuration
+FSLDIR=/usr/local/fsl
+PATH=${FSLDIR}/bin:${PATH}
+source ${FSLDIR}/etc/fslconf/fsl.sh
+export FSLDIR PATH
---
+++ postprocessing/tract_specific/anterior_commissure.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+# Usage: sh <this_script> /fully/qualified/path/to/fibfile.fib.gz
+#
+# Lots of assumptions being made here about how files are named and stored 
+# after the manual part of the protocol.
+
+# Structure names, long and short form
+STRUCTURE=anterior_commissure
+STRUCT=ac
+
+# Get DSI Studio binary location, and tractography options. Then initialize 
+# file and directory names.
+source ../dsi_studio_setup.sh
+source ../get_dir_names.sh "${1}" "${STRUCTURE}"
+
+# For a few tracts, e.g. anterior commissure, we need to combine multiple seed 
+# images
+SEED="${OUTDIR}"/"${STRUCT}"_LR_seed.nii.gz
+fslmaths "${STRUCTDIR}"/"${STRUCT}"_L_seed1.nii.gz \
+	-add "${STRUCTDIR}"/"${STRUCT}"_R_seed1.nii.gz \
+	-bin "${SEED}"
+
+# Compute tracts and export density map. This command line is structure-specific in terms of ROIs etc.
+$DSI_STUDIO \
+	--action=trk \
+	--source=$FIB \
+	--roa="${STRUCTDIR}"/"${STRUCT}"_ROA1.nii.gz \
+	--seed="${SEED}" \
+	${DSI_OPTION_STRING} \
+	--output="${OUTDIR}"/"${STRUCT}".trk.gz \
+	--export=tdi \
+	> "${LOGFILE}" 2>&1
+
+# Give the density map output a clearer filename
+mv "${OUTDIR}"/"${STRUCT}".trk.gz.tdi.nii.gz \
+	"${OUTDIR}"/"${STRUCT}"_density_tdi.nii.gz \
+	>> "${LOGFILE}" 2>&1
+
+
