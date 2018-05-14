af9023a6a1f3118b32d9479969ea078f69d4ff1c
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2018-05-14 12:08:15 -0500

    Updated three bilat tracts

--- postprocessing/tract_specific/run_all.sh
+++ postprocessing/tract_specific/run_all.sh
@@ -18,7 +18,6 @@ sh seed1_LR_roa1s_roi0.sh "${FIB}"       posterior_limb_internal_capsule pic
 sh seed1_LR_roa1s_roi0.sh "${FIB}"       sagittal_stratum ss
 sh seed1_LR_roa1s_roi0.sh "${FIB}"       superior_corona_radiata scr
 sh seed1_LR_roa1s_roi0.sh "${FIB}"       superior_longitudinal_fasciculus slf
-sh seed1_LR_roa1s_roi0.sh "${FIB}"       tapetum_corpus_callosum tap
 sh seed1_LR_roa1s_roi0.sh "${FIB}"       uncinate_fasciculus unc
 
 # Left and right tracts, 1 seed each, 1 ROI each, single shared ROA
@@ -26,7 +25,6 @@ sh seed1_LR_roa1s_roi1.sh "${FIB}"       corticospinal_tract cst
 sh seed1_LR_roa1s_roi1.sh "${FIB}"       fornix_stra_terminalis fxst
 sh seed1_LR_roa1s_roi1.sh "${FIB}"       inferior_cerebellar_peduncle icp
 sh seed1_LR_roa1s_roi1.sh "${FIB}"       pontine_crossing_tract pct
-sh seed1_LR_roa1s_roi1.sh "${FIB}"       superior_cerebellar_peduncle scp
 sh seed1_LR_roa1s_roi1.sh "${FIB}"       superior_fronto_occipital_fasciculus sfo
 
 # Left and right tracts, 1 seed each, 2 ROIs each, single shared ROA
@@ -47,10 +45,8 @@ sh seed1_LR_roa1s_roi0_end2.sh "${FIB}"  temporal_lobe tl
 
 # Left and right tracts, 2 seeds each, single shared ROA
 sh seed2_LR_roa1s_roi0.sh "${FIB}"       cingulum_cingulate_gyrus cgc
-sh seed2_LR_roa1s_roi0.sh "${FIB}"       optic_tract opt
 
 # Left and right tracts, 2 seeds each, 1 ROI each, single shared ROA
-# FIXME Something wrong here. Options not picked up correctly, seed file not found (but is present after), trk file saved in wrong place, no density image
 sh seed2_LR_roa1s_roi1.sh "${FIB}"       posterior_thalamic_radiation ptr
 
 # Single bilateral tract, 1 seed, 1 ROA
@@ -62,15 +58,7 @@ sh seed1_bilat_roa1_roi0.sh "${FIB}"     splenium_corpus_callosum scc
 # Single bilateral tract, 2 seeds, 1 ROA
 sh seed2_bilat_roa1_roi0.sh "${FIB}"     anterior_commissure ac
 sh seed2_bilat_roa1_roi0.sh "${FIB}"     middle_cerebellar_peduncle mcp
-
-
-
-
-
-
-
-
-
-
-
+sh seed2_bilat_roa1_roi0.sh "${FIB}"     tapetum_corpus_callosum tap
+sh seed2_bilat_roa1_roi0.sh "${FIB}"     superior_cerebellar_peduncle scp
+sh seed2_bilat_roa1_roi0.sh "${FIB}"     optic_tract opt
 
